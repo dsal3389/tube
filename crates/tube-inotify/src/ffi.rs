576776435d32c2ab1773344d3392eb7742282ccf
daniel sonbolian <dsal3389@gmail.com>
2024-10-01 02:13:56 +0300

    added couple of function to inotify struct

--- crates/tube-inotify/src/ffi.rs
+++ crates/tube-inotify/src/ffi.rs
@@ -1,9 +1,18 @@
 use std::os::raw::{c_char, c_int};
 
+#[repr(C)]
+pub struct inotify_event {
+    pub wd: c_int,
+    pub mask: u32,
+    pub cookie: u32,
+    pub len: u32,
+}
+
 extern "C" {
     pub(crate) fn inotify_init() -> c_int;
     pub(crate) fn inotify_init1(flags: c_int) -> c_int;
     pub(crate) fn inotify_add_watch(fd: c_int, pathname: *const c_char, mask: u32) -> c_int;
     pub(crate) fn inotify_rm_watch(fd: c_int, wd: c_int) -> c_int;
+    pub(crate) fn read(fd: c_int, buf: *mut u8, count: usize) -> usize;
     pub(crate) fn close(fd: c_int) -> c_int;
 }
--- crates/tube-inotify/src/lib.rs
+++ crates/tube-inotify/src/lib.rs
@@ -1,8 +1,11 @@
+use futures::stream::Stream;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
 use std::os::raw::c_int;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 mod ffi;
 
@@ -13,6 +16,7 @@ use ffi::inotify_rm_watch;
 #[derive(Debug, PartialEq)]
 pub enum InotifyError {
     InitError,
+    NotDirectory { path: PathBuf },
     ENVAL,
     EMFILE,
     ENOMEM,
@@ -22,6 +26,7 @@ impl fmt::Display for InotifyError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::InitError => write!(f, "inotify init error"),
+            Self::NotDirectory { path } => write!(f, "given path `{:?}` is not a directory", path),
             Self::ENVAL => write!(f, "An invalid value was specified in flags"),
             Self::EMFILE => write!(
                 f,
@@ -35,24 +40,29 @@ impl fmt::Display for InotifyError {
 impl Error for InotifyError {}
 
 /// Inotify struct contians the information about
-/// the invoked InotifyError
-///
+/// the invoked InotifyError,
 /// this method types is builder pattern
 #[derive(Debug)]
 pub struct Inotify {
     fd: c_int,
-    watchers: HashMap<c_int, InotifyWatch>,
+    watchers: HashMap<i32, InotifyWatch>,
 }
 
-/// InotifyWatch cannot be created directly, it can only
-/// be created with the `Inotify` methods.
-///
-///
+/// InotifyWatch, a high level class that holds the inotify watcher
+/// information like the inotify file descriptor, watcher descriptor
+/// and the path that watcher is watching
 #[derive(Debug)]
 pub struct InotifyWatch {
     fd: c_int,
     wd: c_int,
     pathname: PathBuf,
+}
+
+pub struct InotifyEvent {
+    wd: i32,
+    mask: u32,
+    cookie: u32,
+    len: u32,
 }
 
 impl Inotify {
@@ -67,7 +77,7 @@ impl Inotify {
         })
     }
 
-    /// watch the specified pathname, method takes ownershipt of `self`
+    /// addes a path to the inotify watch event via `inotify_add_watch`
     pub fn watch(mut self, pathname: PathBuf) -> Result<Self, InotifyError> {
         let wd = unsafe {
             ffi::inotify_add_watch(self.fd, pathname.to_str().unwrap().as_ptr() as *const i8, 0)
@@ -80,9 +90,59 @@ impl Inotify {
         self.watchers.insert(wd, watcher);
         Ok(self)
     }
+
+    /// this function is for directories, since I notify is not recursive
+    /// listening for events on directories won't trigger events for sub directories
+    /// the depth arguments defines how deep the recurse the given directory, if None is given
+    /// then there is no limit
+    pub fn watch_recursive(
+        mut self,
+        pathname: PathBuf,
+        depth: Option<u32>,
+    ) -> Result<Self, InotifyError> {
+        if !pathname.is_dir() {
+            return Err(InotifyError::NotDirectory { path: pathname });
+        }
+
+        for entry in pathname.read_dir().expect("couldn't read directory") {
+            match entry {
+                Ok(entry) if entry.path().is_dir() => {
+                    // add the directory to the watch list and decrease the depths by one
+                    self = self.watch_recursive(entry.path(), depth.and_then(|n| Some(n - 1)))?;
+                }
+                _ => todo!(),
+            }
+        }
+        Ok(self)
+    }
+
+    /// returns reference to the `watch` instance by the given
+    /// watch descriptor
+    pub fn get_watch_by_descriptor(&self, wd: i32) -> Option<&InotifyWatch> {
+        self.watchers.get(&wd)
+    }
 }
 
-/// octal formatting returns the Inotify file descriptor
+impl InotifyWatch {
+    /// returns the defined path for the watcher
+    pub fn pathname(&self) -> &Path {
+        &self.pathname
+    }
+}
+
+impl Stream for Inotify {
+    type Item = InotifyEvent;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        unsafe {
+            let mut event_ptr = [0u8; 1];
+            let read_len = ffi::read(self.fd, event_ptr.as_mut_ptr(), 1);
+        }
+        todo!()
+    }
+}
+
+/// octal formatting for Inotify returns the Inotify file descriptor
 impl fmt::Octal for Inotify {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.fd)
@@ -103,8 +163,7 @@ impl fmt::Display for InotifyWatch {
     }
 }
 
-/// clean all resources when inotify is dropped, also drop
-/// the watchers
+/// clean all resources when inotify is dropped, also drop the watchers
 impl Drop for Inotify {
     fn drop(&mut self) {
         self.watchers.drain();
